1ca2ccb85554bb6c9051b531a40583b4bef9816a
cyoure <152484982+cyoure@users.noreply.github.com>
2024-12-06 01:27:56 +0900

    [Bronze V] Title: A/B, Time: 188 ms, Memory: 17980 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Double A = sc.nextDouble();
+        Double B = sc.nextDouble();
+
+        System.out.println(A/B);
+    }
+}
