1b8414a5076d5d5e0ef6e3594df2c8e7b977dfdf
cyoure <152484982+cyoure@users.noreply.github.com>
2024-12-19 15:29:41 +0900

    [Bronze V] Title: A+B - 8, Time: 224 ms, Memory: 18948 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int[] sum = new int[T];
+        int[] arrayA = new int[T];
+        int[] arrayB = new int[T];
+
+
+        for (int i = 0; i  < T; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            arrayA[i] = A;
+            arrayB[i] = B;
+            sum[i] = A + B;
+        }
+        for (int i = 0; i < sum.length; i++) {
+            System.out.println("Case #" + (i + 1) + ": " + arrayA[i] + " + " + arrayB[i] + " = " + sum[i]);
+
+        }
+    }
+}
+
