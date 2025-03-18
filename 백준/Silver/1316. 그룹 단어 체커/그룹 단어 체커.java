e8ccd3dce468a1f69585276413bf8102446a1124
cyoure <152484982+cyoure@users.noreply.github.com>
2025-03-18 18:15:55 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 180 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -2,38 +2,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            int N = sc.nextInt();
-            int cnt = 0;
-
-            for (int i = 0; i < N; i++) {
-                String word = sc.next();
-                boolean[] checked = new boolean[26];  // 알파벳 체크용 배열
-                char prev = '\0';  // 이전 문자
-                boolean isGroup = true;
-
-                for (int j = 0; j < word.length(); j++) {
-                    char now = word.charAt(j);
-
-                    // 이전 문자와 다를 때
-                    if (now != prev) {
-                        // 이미 나온 문자면 그룹 단어가 아님
-                        if (checked[now - 'a']) {
-                            isGroup = false;
-                            break;
-                        }
-                        // 처음 나온 문자면 체크하고 진행
-                        checked[now - 'a'] = true;
-                        prev = now;
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            String term = sc.next();
+            boolean check = true;
+            for (int j = 0; j < term.length(); j++) {
+                char current = term.charAt(j);
+                while (j+1 < term.length() && term.charAt(j+1) == current) {
+                    j++;
+                }
+                for (int k = j+1; k < term.length(); k++) {
+                    if (term.charAt(k) == current) {
+                        check = false;
+                        break;
                     }
                 }
-
-                if (isGroup) {
-                    cnt++;
+                if (!check) {
+                    break;
                 }
             }
-
-            System.out.println(cnt);
-            sc.close();
+            if (check) {
+                count++;
+            }
         }
+        System.out.println(count);
     }
+}
